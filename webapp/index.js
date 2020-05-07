87d8c71664725b5bfb0d7ec0573f03b110085ecc
Áron Kósa <kosa.aron98@gmail.com>
2020-05-07 10:51:12 +0200

    initial commit

---
+++ webapp/index.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
+	XMLView.create({
+		viewName: "20200416.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+});
